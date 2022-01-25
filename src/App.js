7babfcb03aa1e1d3e1f3f09adcead393f3f1b491
yunghsin Lee <lenalee7963@gmail.com>
2022-01-25 21:31:37 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Main Homepage
         </a>
       </header>
     </div>
